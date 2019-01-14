a5e8e16c6ff1b86226439444060ba34df865fbc7
Nevin John <nevin.john.13@gmail.com>
2019-01-15 00:05:33 +0530

    Add files via upload

--- lib/views/pezhamattom_view.dart
+++ lib/views/pezhamattom_view.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:kgs_app/services/Authentication.dart';
 
 class PezhamattomView extends StatefulWidget {
   final String title;
+
 
   PezhamattomView({this.title});
 
@@ -13,6 +17,55 @@ class PezhamattomView extends StatefulWidget {
 }
 
 class _PezhamattomViewState extends State<PezhamattomView> {
+
+
+  Future verifyContact() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new PezhamattomContacts(title: 'Pezhamattom Contacts')));
+    } else {
+
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new LoginSignUpPage(auth: new Auth(),prefs: prefs)));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    new Timer(new Duration(milliseconds: 200), () {
+      verifyContact();
+    });
+  }
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Text('Loading...'),
+      ),
+    );
+  }
+
+
+}
+
+class PezhamattomContacts extends StatefulWidget {
+  final String title;
+
+  PezhamattomContacts({this.title});
+
+  @override
+  _PezhamattomContactsState createState() => _PezhamattomContactsState();
+}
+
+
+
+class _PezhamattomContactsState extends State<PezhamattomContacts> {
   String _phone = '';
   List allContacts = [];
 
@@ -87,119 +140,364 @@ class _PezhamattomViewState extends State<PezhamattomView> {
                   height: 600.0,
                   width: 500.0,
                   decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
+                  BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
                   child: SingleChildScrollView(
                       child: Column(
-                    children: <Widget>[
-                      Stack(
                         children: <Widget>[
-                          Container(height: 250.0),
-                          Container(
-                            height: 270.0,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10.0),
-                                  topRight: Radius.circular(10.0),
-                                ),
-                                image: DecorationImage(
-                                    image: CachedNetworkImageProvider(
-                                        contact['picUrl']),
-                                    fit: BoxFit.cover)),
+                          Stack(
+                            children: <Widget>[
+                              Container(height: 250.0),
+                              Container(
+                                height: 270.0,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(10.0),
+                                      topRight: Radius.circular(10.0),
+                                    ),
+                                    image: DecorationImage(
+                                        image: CachedNetworkImageProvider(
+                                            contact['picUrl']),
+                                        fit: BoxFit.cover)),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      SizedBox(height: 10.0),
-                      Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text(
-                            contact['name'],
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )),
-                      SizedBox(height: 5.0),
-                      Padding(
-                          padding: EdgeInsets.only(bottom: 10.0),
-                          child: Text(
-                            contact['desc'],
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              height: 1.0,
-                              wordSpacing: 2.0,
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.w300
-                            ),
-                          )),
+                          SizedBox(height: 10.0),
+                          Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(
+                                contact['name'],
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )),
+                          SizedBox(height: 5.0),
+                          Padding(
+                              padding: EdgeInsets.all( 10.0),
+                              child: Text(
+                                contact['desc'],
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    height: 1.0,
+                                    wordSpacing: 2.0,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.w300
+                                ),
+                              )),
 
-                      SizedBox(height: 10.0),
-                      Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text(
-                            'Phonenumber - '+contact['phoneNumber'],
-                            style: TextStyle(
-                              fontFamily: 'Montserrat',
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w100,
-                            ),
-                          )),
-                      SizedBox(height: 30.0),
-                      Container(
-                          height: 40.0,
-                          width: 95.0,
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20.0),
-                            shadowColor: Colors.greenAccent,
-                            color: Colors.deepOrangeAccent,
-                            elevation: 7.0,
-                            child: GestureDetector(
-                              onTap: () => setState(() {
+                          SizedBox(height: 10.0),
+                          Padding(
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(
+                                'Phonenumber - '+contact['phoneNumber'],
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w100,
+                                ),
+                              )),
+                          SizedBox(height: 30.0),
+                          Container(
+                              height: 40.0,
+                              width: 95.0,
+                              child: Material(
+                                borderRadius: BorderRadius.circular(20.0),
+                                shadowColor: Colors.greenAccent,
+                                color: Colors.deepOrangeAccent,
+                                elevation: 7.0,
+                                child: GestureDetector(
+                                  onTap: () => setState(() {
                                     _launched = _makePhoneCall(
                                         'tel://' + contact['phoneNumber']);
                                   }),
-                              child: Center(
-                                child: Text(
-                                  'Call',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: 'Montserrat'),
+                                  child: Center(
+                                    child: Text(
+                                      'Call',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'Montserrat'),
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                          )),
-                      SizedBox(height: 15.0),
-                      Container(
-                          height: 40.0,
-                          width: 130.0,
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20.0),
-                            shadowColor: Colors.greenAccent,
-                            color: Colors.green,
-                            elevation: 7.0,
-                            child: GestureDetector(
-                              onTap: () => setState(() {
+                              )),
+                          SizedBox(height: 15.0),
+                          Container(
+                              height: 40.0,
+                              width: 130.0,
+                              child: Material(
+                                borderRadius: BorderRadius.circular(20.0),
+                                shadowColor: Colors.greenAccent,
+                                color: Colors.green,
+                                elevation: 7.0,
+                                child: GestureDetector(
+                                  onTap: () => setState(() {
                                     _launched = _makePhoneCall(
                                         'whatsapp://send?phone=' +
                                             contact['phoneNumber']);
                                   }),
-                              child: Center(
-                                child: Text(
-                                  'Whatsapp',
-                                  style: TextStyle(
+                                  child: Center(
+                                    child: Text(
+                                      'Whatsapp',
+                                      style: TextStyle(
 
 
-                                      color: Colors.white,
-                                      fontFamily: 'Montserrat'),
+                                          color: Colors.white,
+                                          fontFamily: 'Montserrat'),
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                          ))
-                    ],
-                  ))));
+                              ))
+                        ],
+                      ))));
         });
   }
 }
+
+
+
+class LoginSignUpPage extends StatefulWidget {
+  SharedPreferences prefs;
+  LoginSignUpPage({this.auth, this.onSignedIn,this.prefs});
+
+  final BaseAuth auth;
+  final VoidCallback onSignedIn;
+
+  @override
+  State<StatefulWidget> createState() => new _LoginSignUpPageState();
+}
+
+enum FormMode { LOGIN, SIGNUP }
+
+class _LoginSignUpPageState extends State<LoginSignUpPage> {
+  final _formKey = new GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+  String _errorMessage;
+
+  // Initial form is login form
+  FormMode _formMode = FormMode.LOGIN;
+  bool _isIos;
+  bool _isLoading;
+
+  // Check if form is valid before perform login or signup
+  bool _validateAndSave() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  // Perform login or signup
+  _validateAndSubmit() async {
+    setState(() {
+      _errorMessage = "";
+      _isLoading = true;
+    });
+    if (_validateAndSave()) {
+      String userId = "";
+      try {
+        if (_formMode == FormMode.LOGIN) {
+          userId = await widget.auth.signIn(_email, _password);
+          print('Signed in: $userId');
+          widget.prefs.setBool('seen', true);
+          print ('ShareedPreferencesNevins Value :  $widget.seen');
+
+        } else {
+          userId = await widget.auth.signUp(_email, _password);
+          print('Signed up user: $userId');
+        }
+        setState(() {
+          _isLoading = false;
+        });
+
+        if (userId.length > 0 && userId != null) {
+          widget.onSignedIn();
+        }
+
+      } catch (e) {
+        print('Error: $e');
+        setState(() {
+          _isLoading = false;
+          if (_isIos) {
+            _errorMessage = e.details;
+          } else
+            _errorMessage = e.message;
+        });
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    _errorMessage = "";
+    _isLoading = false;
+    super.initState();
+  }
+
+  void _changeFormToSignUp() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.SIGNUP;
+    });
+  }
+
+  void _changeFormToLogin() {
+    _formKey.currentState.reset();
+    _errorMessage = "";
+    setState(() {
+      _formMode = FormMode.LOGIN;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _isIos = Theme.of(context).platform == TargetPlatform.iOS;
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter login demo'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _showBody(),
+            _showCircularProgress(),
+          ],
+        ));
+  }
+
+  Widget _showCircularProgress(){
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    } return Container(height: 0.0, width: 0.0,);
+
+  }
+
+  Widget _showBody(){
+    return new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+          key: _formKey,
+          child: new ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              _showLogo(),
+              _showEmailInput(),
+              _showPasswordInput(),
+              _showPrimaryButton(),
+              _showSecondaryButton(),
+              _showErrorMessage(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return new Text(
+        _errorMessage,
+        style: TextStyle(
+            fontSize: 13.0,
+            color: Colors.red,
+            height: 1.0,
+            fontWeight: FontWeight.w300),
+      );
+    } else {
+      return new Container(
+        height: 0.0,
+      );
+    }
+  }
+
+  Widget _showLogo() {
+    return new Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/flutter-icon.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Email',
+            icon: new Icon(
+              Icons.mail,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value,
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Password',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    );
+  }
+
+  Widget _showSecondaryButton() {
+    return new FlatButton(
+      child: _formMode == FormMode.LOGIN
+          ? new Text('Create an account',
+          style: new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300))
+          : new Text('Have an account? Sign in',
+          style:
+          new TextStyle(fontSize: 18.0, fontWeight: FontWeight.w300)),
+      onPressed: _formMode == FormMode.LOGIN
+          ? _changeFormToSignUp
+          : _changeFormToLogin,
+    );
+  }
+
+  Widget _showPrimaryButton() {
+    return new Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+        child: SizedBox(
+          height: 40.0,
+          child: new RaisedButton(
+            elevation: 5.0,
+            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+            color: Colors.blue,
+            child: _formMode == FormMode.LOGIN
+                ? new Text('Login',
+                style: new TextStyle(fontSize: 20.0, color: Colors.white))
+                : new Text('Create account',
+                style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+            onPressed: _validateAndSubmit,
+          ),
+        ));
+  }
+}
+
 
 Future<void> _launched;
 
@@ -207,6 +505,7 @@ Future<void> _makePhoneCall(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
   } else {
+
     throw 'Could not launch $url';
   }
 }
